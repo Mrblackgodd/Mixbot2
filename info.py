ab0901dd5429e16d18df82702556197b2f127815
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2023-10-27 20:12:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,9 +21,9 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "5867415844:AAFAfIZMhjhI6PG3Au0uMZmfvW3VCb8
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/ebad2eb29a3d2702e1531.jpg https://telegra.ph/file/5250452a1ab5364cfc4dc.jpg https://telegra.ph/file/68a707b00e8e88ed3c4a4.jpg https://telegra.ph/file/f44b3f7381cbf2e7708bb.jpg https://telegra.ph/file/4aaa6122c1fe3b71cf52c.jpg https://telegra.ph/file/486a9c2e80ed1729a345f.jpg https://telegra.ph/file/b165f958d038b903d43e1.jpg https://telegra.ph/file/989a744a60c061838444f.jpg https://telegra.ph/file/ff8f67820985179067982.jpg https://telegra.ph/file/3d8c714a3b0c04bfdec7e.jpg https://telegra.ph/file/fd0e1a19b0268c005edf8.jpg https://telegra.ph/file/8cfe0b5ceaa7b969448c4.jpg https://telegra.ph/file/5965fb86394de52b45e31.jpg https://telegra.ph/file/b826e4fabf1bf0fa72db3.jpg https://telegra.ph/file/baba127a57d74bc19e956.jpg')).split() #SAMPLE PIC
-NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/howtodownloadmoviestamilhd/70")
+PICS = (environ.get('PICS', 'https://telegra.ph/file/ec3faaf32d3d4aedc26d0.jpg https://telegra.ph/file/c4828ab57d63f3a726ef3.jpg https://telegra.ph/file/afa309058d5c050766779.jpg')).split() #SAMPLE PIC
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/afa309058d5c050766779.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/afa309058d5c050766779.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
@@ -48,10 +48,10 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "rose9")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', ))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'MENeVZcapqUmOXw9fyRSQm9Z6pu2')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', ))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
@@ -60,7 +60,7 @@ GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+gkhvihXg5K5kM2Rl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/psycobotz')
 TUTORIAL = environ.get('TUTORIAL', 'https://youtube.com/shorts/d7a43WYF5u8?si=bKnhvDt6r0HSTuch')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
-MSG_ALRT = environ.get('MSG_ALRT', 'Hello Nanbha and Nanbis ❤️')
+MSG_ALRT = environ.get('MSG_ALRT', 'Hello SIGMA's')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001748572062'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '-1001818522365')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
